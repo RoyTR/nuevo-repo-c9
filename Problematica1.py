f1ff39222beac0ccdea79f3b4932d2bcd6cc0fe8
Roy Taza <pcisrtaz@upc.edu.pe>
2025-06-21 16:25:31 +0000

    Se hicoieron cambios a pedido del cliente

--- Problematica1.py
+++ Problematica1.py
@@ -1,10 +1,12 @@
 import math
 
 def main():
-    radio = float(input("Ingrese el radio del criculo: "))
     
-    area_del_circulo = math.pi * math.pow(radio, 2)
+    
+    #area_del_circulo = math.pi * math.pow(radio, 2)
     
     print(f"El area del circulo es: {area_del_circulo}")
+    
+    #agreggo este comentario adicoinal
     
 main()
