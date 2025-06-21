41b397d3982e6637eb9562d4f3c608acf93e17cc
Roy Taza <pcisrtaz@upc.edu.pe>
2025-06-21 16:28:53 +0000

    Varios cambios realizados

--- Problematica1.py
+++ Problematica1.py
@@ -3,7 +3,7 @@ import math
 def main():
     
     
-    #area_del_circulo = math.pi * math.pow(radio, 2)
+    area_del_circulo = math.pi * math.pow(radio, 2)
     
     print(f"El area del circulo es: {area_del_circulo}")
     
