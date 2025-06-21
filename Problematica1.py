20b51a2a97a866106f6e59e766c1d7c347397505
Roy Taza <pcisrtaz@upc.edu.pe>
2025-06-21 16:23:39 +0000

    Agregue la solucion en codigo python

--- Problematica1.py
+++ Problematica1.py
@@ -1,1 +1,10 @@
-#Aca la primera problematica
+import math
+
+def main():
+    radio = float(input("Ingrese el radio del criculo: "))
+    
+    area_del_circulo = math.pi * math.pow(radio, 2)
+    
+    print(f"El area del circulo es: {area_del_circulo}")
+    
+main()
